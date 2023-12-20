1de237e7d2739b888c4f220b9820686f7e1ccbcc
Khanhthi12 <153153664+Khanhthi12@users.noreply.github.com>
2023-12-20 23:20:54 +0700

    Create Practice_13.sql

---
+++ Practice_13.sql
@@ -0,0 +1,15 @@
+--EX01
+SELECT 
+COUNT(DISTINCT company_id) AS duplicate_companies
+FROM (
+  SELECT 
+    company_id, 
+    title, 
+    description, 
+    COUNT(job_id) AS job_count
+  FROM job_listings
+  GROUP BY company_id, title, description
+) AS job_count_cte
+WHERE job_count > 1;
+
+--EX02
